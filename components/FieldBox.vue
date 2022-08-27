1effcbed6a2814716d834fe4ea7bfb60ec92f83c
lokimenethill <xdjtrancemetal@hotmail.com>
2022-08-27 17:00:57 -0500

    enhanced_split_to_replace_functions

--- components/FieldBox.vue
+++ components/FieldBox.vue
@@ -15,7 +15,7 @@
           label="Inserta el texto del chat de League of Legends"
         ></v-textarea>
         Datos en una linea:
-        <p>{{outputText}}</p>
+        <p v-show="!isActive">{{outputText}}</p>
         
         <v-btn @click="find()" color="info" :disabled="isActive">Buscar</v-btn>
         <span v-show="isActive">Faltan datos de busqueda</span>
@@ -33,66 +33,51 @@ data(){
         inputText:'',
         regionSelected:'',
         region: ['lan', 'las','na'],
-        dataTrashStringLang:{
-          es:['se','unió','a','la','sala'],
-          en:[],
-        },
         lang:0,
         isActive:true
     }
 },
 computed:{
         outputText(){
-          let selectorArrayLang=[];
+          let replacedText ='';
             if(this.regionSelected=="lan" || this.regionSelected=="las"){
-              selectorArrayLang=this.dataTrashStringLang['es'];
+              replacedText=this.inputText.replace(/se unió a la sala/g,",");
             }else{
-              selectorArrayLang=this.dataTrashStringLang['en'];
+              replacedText=this.inputText.replace(/joined the lobby/g,",");
             }
-            let cleanedText = this.inputText.replace(/(\r\n|\n|\r)/gm, " ");
-            let summonerArray=cleanedText.split(" ");
+            let cleanedText = replacedText.replace(/(\r\n|\n|\r)/gm, "");
+            let summonerArray=cleanedText.split(",");
             let summonerNames=[]
             let summonerNamesOneLiner = '';
-            //console.log(summonerArray)
-            for(let i=0;i<selectorArrayLang.length;i++){
-              summonerNames=summonerArray.filter(word => word != selectorArrayLang[i]);
-              summonerArray=summonerNames;
-            }
-            for(let i=0;i<summonerNames.length;i++){
-              if(i==summonerNames.length-1){
-                summonerNamesOneLiner+=summonerNames[i];
-              }else{
-                summonerNamesOneLiner+=summonerNames[i]+',';
+            for(let i=0;i<summonerArray.length;i++){
+              if(summonerArray[i]!=""){
+                summonerNames.push(summonerArray[i]);
+                summonerNamesOneLiner+=summonerArray[i]+','
               }
             }
+           //console.log(summonerNames);
             return summonerNamesOneLiner;
         },
     },
     methods:{
     find(){
-      let selectorArrayLang=[];
+            let replacedText ='';
             if(this.regionSelected=="lan" || this.regionSelected=="las"){
-              selectorArrayLang=this.dataTrashStringLang['es'];
+              replacedText=this.inputText.replace(/se unió a la sala/g,",");
             }else{
-              selectorArrayLang=this.dataTrashStringLang['en'];
+              replacedText=this.inputText.replace(/joined the lobby/g,",");
             }
-       let cleanedText = this.inputText.replace(/(\r\n|\n|\r)/gm, " ");
-            let summonerArray=cleanedText.split(" ");
+            let cleanedText = replacedText.replace(/(\r\n|\n|\r)/gm, "");
+            let summonerArray=cleanedText.split(",");
             let summonerNames=[]
             let summonerNamesOneLiner = '';
-            console.log(summonerArray)
-            for(let i=0;i<selectorArrayLang.length;i++){
-              summonerNames=summonerArray.filter(word => word != selectorArrayLang[i]);
-              summonerArray=summonerNames;
-            }
-            for(let i=0;i<summonerNames.length;i++){
-              if(i==summonerNames.length-1){
-                summonerNamesOneLiner+=summonerNames[i];
-              }else{
-                summonerNamesOneLiner+=summonerNames[i]+',';
+            for(let i=0;i<summonerArray.length;i++){
+              if(summonerArray[i]!=""){
+                summonerNames.push(summonerArray[i]);
+                summonerNamesOneLiner+=summonerArray[i]+','
               }
             }
-            console.log(summonerNames)
+           console.log(summonerNames);
             let tempUrl=''
             for(let x=0;x<summonerNames.length;x++){
               tempUrl=`https://www.leagueofgraphs.com/summoner/${this.regionSelected}/${summonerNames[x]}`
